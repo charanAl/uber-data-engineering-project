abbb56b32efd4ec624c89e1800087dd8e326c978
Alle Charan <159403982+charanAl@users.noreply.github.com>
2026-09-11 09:14:30 +0530

    Add configuration constants for GCP and datasets

--- src/config.py
+++ src/config.py
@@ -1,1 +1,16 @@
 
+import os
+
+PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+
+BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
+
+DATASET_BRONZE = "uber_bronze"
+
+DATASET_SILVER = "uber_silver"
+
+DATASET_GOLD = "uber_gold"
+
+RAW_TABLE = "uber_raw"
+
+CLEAN_TABLE = "uber_clean"
